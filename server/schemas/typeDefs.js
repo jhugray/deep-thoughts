2188ff5e60ccb2b688049a89149b51ccfdfa1fcb
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-13 15:18:21 -0500

    21.5

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Auth {
-    token: ID!
-    user: User
-  }
   type User {
     _id: ID
     username: String
@@ -30,6 +26,11 @@ const typeDefs = gql`
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me: User
     users: [User]
@@ -37,6 +38,7 @@ const typeDefs = gql`
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
